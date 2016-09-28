5d59bd6e2f375450cd6ee05f0c870469931651db
w1-vanspengenm <marco.vanspengen@edu-kw1c.nl>
2016-09-28 14:03:35 +0200

    commit 2

--- JS/myScript.js
+++ JS/myScript.js
@@ -1,4 +1,4 @@
-var json;
+var j;
 function handleFile(e) {
   var files = e.target.files;
   var i,f;
@@ -14,30 +14,30 @@ function handleFile(e) {
         var first_sheet_name = workbook.SheetNames[0];
         /*sla dat werkbland op in var*/
         var worksheet = workbook.Sheets[first_sheet_name];
-        json = XLSX.utils.sheet_to_json(worksheet);
-        console.log(json);
+        j = XLSX.utils.sheet_to_json(worksheet);
+        console.log(j);
     };
     reader.readAsBinaryString(f);
   }
 }
 $(document).ready(function ()
 {
-    $("#Dropzone").on("dragover", function (e)
+    $("#dropzone").on("dragover", function (e)
     {
         e.preventDefault();
         $(this).addClass("selected");
         $(this).text("Laat muis los.");
     });
-    $("#Dropzone").on("dragleave", function (e)
+    $("#dropzone").on("dragleave", function (e)
     {
         e.preventDefault();
         $(this).removeClass("selected");
-        $(this).text("Sleep het (Exel) bestand hierin.");
+        $(this).text("Sleep het (Excel) bestand hierin.");
     });
-    $("#Dropzone").on("mouseleave", function ()
+    $("#dropzone").on("mouseleave", function ()
     {
         $(this).removeClass("selected");
-        $(this).text("Sleep het (Exel) bestand hierin.");
+        $(this).text("Sleep het (Excel) bestand hierin.");
     });
 });
 
@@ -53,22 +53,33 @@ function handleDrop(e) {
     if(files.length!=1)
     
     {
-        $("#Dropzone").text("Gebruik 1 bestand tegelijk!");
+        $("#output").html("<h2>Gebruik 1 bestand tegelijk!</h2>");
     }
     else
     
     {
-            reader.onload = function(e) {
+      reader.onload = function(e) {
       var data = e.target.result;
       var workbook = XLSX.read(data, {type: 'binary'});
         /*achterhaal de naam van het eerste werkblad*/
         var first_sheet_name = workbook.SheetNames[0];
         /*sla dat werkbland op in var*/
         var worksheet = workbook.Sheets[first_sheet_name];
-        json = XLSX.utils.sheet_to_json(worksheet);
-        console.log(json);
+        j= XLSX.utils.sheet_to_json(worksheet);
+        console.log(j);
     };
     reader.readAsBinaryString(f);
     }
   }
+}
+outputJSON(j);
+
+function outputJSON(data)
+
+{
+    for(i=0; i<data.length; i++ )
+    
+    {
+      document.write(data.adres1)  
+    }
 }
